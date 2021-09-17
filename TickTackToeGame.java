0856e7f4e8bf658184b281d7d15da3a5f0e89753
Ritik Chaurasiya <chaurasiyaritik50@gmail.com>
2021-09-17 17:05:34 +0530

    Added Comment in the program

--- TickTackToeGame.java
+++ TickTackToeGame.java
@@ -3,11 +3,11 @@ package com.workshop;
 public class TicTackToeGame {
     public static void main(String[] args) {
         System.out.println("Welcome To Tic Tac Toe GAme!!!!");
-        createBoard();
+        createBoard();				//Calling the createBoard function
     }
     public static void createBoard(){
         char[] board=new char[10];
-        for(int index=1;index<=9;index++){
+        for(int index=1;index<=9;index++){      // making all position of ticktacktoegame to null
             board[index]=' ';
         }
     }
